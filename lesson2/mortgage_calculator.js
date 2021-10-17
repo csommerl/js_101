85a82845ca8764b8a8860edfd39125cc79ab5fb1
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2021-10-17 11:49:13 -0400

    Finish initial mortgage calculator

--- lesson2/mortgage_calculator.js
+++ lesson2/mortgage_calculator.js
@@ -2,3 +2,25 @@
 ** Assignment: Mortgage / Car Loan Calculator
 ** https://launchschool.com/lessons/64655364/assignments/fb1e377c
 */
+
+let readline = require('readline-sync');
+
+// Get user information
+console.log('Enter the total amount of the loan.');
+let loanAmount = Number(readline.prompt());
+
+console.log('Enter the Annual Percentage Rate (APR) as a percentage.');
+let aprPercentage = Number(readline.prompt());
+
+console.log('Enter the duration of the loan in years');
+let durationInYears = Number(readline.prompt());
+
+// Convert to right types
+let monthlyRate = (aprPercentage / 12) / 100;
+let durationInMonths = durationInYears * 12;
+
+// calculate
+let monthlyPayment = loanAmount *
+  (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-durationInMonths))));
+
+console.log(`Your monthly payment would be $${monthlyPayment.toFixed(2)}.`);
