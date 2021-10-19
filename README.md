b8e679d21f24e047b4a9afef81fbc106849a74fc
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2021-10-19 10:03:22 -0400

    Update Mortgage Calculator from Code Review feedback

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 # Resources
 - [SPOT Wiki](https://www.notion.so/SPOT-Wiki-1b4fe53778f34614b28a8e76ce028782)
 - [JS101 Exercises](https://www.notion.so/JS101-085f4f55ed5e41cb9003d9f0a3aa48ef)
+
+# How to set up npm
+https://launchschool.com/books/javascript/read/input_output#commandlineinput
+1. Use the ls command to see if there is a `package.json` file in the current directory.
+2. If there is not one, create one with `npm init -y`.
+3. Install what you need, e.g., `npm install readline-sync --save`
+
+# How to set up ESLint
+https://launchschool.com/gists/9ad96eed
+1. Install: ```npm install eslint@7.12.1 eslint-cli babel-eslint --save-dev```
+2. Check version: ```npx eslint -v```
+3. Put your default `.eslintrc.yml` file in a directory that contains all of your projects as subdirectories. Alternatively, you can put the file in each project directory and customize it as needed.
+4. run eslint: ```npx eslint test.js```
--- lesson2/mortgage_calculator.js
+++ lesson2/mortgage_calculator.js
@@ -5,64 +5,82 @@
 
 let readline = require('readline-sync');
 
-function invalidNumber(num) {
-  return  Number.isNaN(num) ||
-          num < 0;
-}
-
-console.log('Welcome to the mortgage calculator!');
-
-while (true) {
-  // Get user information
+function getLoanAmount() {
   console.log('Enter the total amount of the loan.');
   let loanAmount = Number(readline.prompt());
 
-  while (invalidNumber(loanAmount)) {
+  while (Number.isNaN(loanAmount) || loanAmount <= 0) {
     console.log('Invalid input. You must enter a positive number. Try again.');
     loanAmount = Number(readline.prompt());
   }
 
+  return loanAmount;
+}
+
+function getMonthlyRate() {
   console.log('Enter the Annual Percentage Rate (APR) as a percentage.');
   let aprPercentage = Number(readline.prompt());
 
-  while (invalidNumber(aprPercentage)) {
+  while (Number.isNaN(aprPercentage) || aprPercentage < 0) {
     console.log('Invalid input. You must enter a positive number. Try again.');
     aprPercentage = Number(readline.prompt());
   }
 
-  console.log('Enter the duration of the loan in years');
+  let monthlyRate = (aprPercentage / 12) / 100;
+  return monthlyRate;
+}
+
+function getLoanDuration() {
+  console.log('Enter the duration of the loan in years.');
   let durationInYears = Number(readline.prompt());
 
-  while (invalidNumber(durationInYears)) {
-    console.log('Invalid input. You must enter a positive number. Try again.');
+  while (!Number.isInteger(durationInYears) || durationInYears <= 0) {
+    console.log('Invalid input. Enter a positive integer. Try again.');
     durationInYears = Number(readline.prompt());
   }
 
-  // Convert to right types
-  let monthlyRate = (aprPercentage / 12) / 100;
   let durationInMonths = durationInYears * 12;
+  return durationInMonths;
+}
 
-  // calculate
+function getMonthlyPayment(loanAmount, monthlyRate, loanDuration) {
   let monthlyPayment;
-  if (aprPercentage === 0) {
-    monthlyPayment = loanAmount / durationInMonths;
+  if (monthlyRate === 0) {
+    monthlyPayment = loanAmount / loanDuration;
   } else {
-    monthlyPayment =  loanAmount *
-                      (monthlyRate /
-                      (1 - Math.pow((1 + monthlyRate), (-durationInMonths))));
+    monthlyPayment = loanAmount *
+      (monthlyRate / (1 - Math.pow((1 + monthlyRate), (-loanDuration))));
   }
+  return monthlyPayment;
+}
 
-  // display result
+function displayFinalAmounts(monthlyPayment) {
   console.log(`Your monthly payment would be $${monthlyPayment.toFixed(2)}.`);
+}
 
-  // conclude iteration
+function askForAnotherCalculation() {
   console.log('Would you like to calculate another mortgage payment? (y/n)');
   let anotherCalculation = readline.prompt().toLowerCase();
-  while (anotherCalculation[0] !== 'y' && anotherCalculation[0] !== 'n') {
-    console.log('Please enter y or n.');
+  while (anotherCalculation !== 'y' && anotherCalculation !== 'n') {
+    console.log('Please enter a single letter that is either "y" or "n".');
     anotherCalculation = readline.prompt().toLowerCase();
   }
-  if (anotherCalculation[0] === 'n') {
+  return anotherCalculation;
+}
+
+// Main program
+console.log('Welcome to the mortgage calculator!');
+while (true) {
+
+  let loanAmount = getLoanAmount();
+  let monthlyRate = getMonthlyRate();
+  let loanDuration = getLoanDuration();
+
+  let monthlyPayment = getMonthlyPayment(loanAmount, monthlyRate, loanDuration);
+  displayFinalAmounts(monthlyPayment);
+
+  let anotherCalculation = askForAnotherCalculation();
+  if (anotherCalculation === 'n') {
     console.log('Have a good day!');
     break;
   }
