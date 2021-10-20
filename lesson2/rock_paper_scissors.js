c28e2701f8f00edf921622f547e9bf172223ab5c
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2021-10-19 22:19:43 -0400

    Refactor with displayWinner

--- lesson2/rock_paper_scissors.js
+++ lesson2/rock_paper_scissors.js
@@ -11,18 +11,7 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question();
-
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt('That\'s not a valid choice');
-    choice = readline.question();
-  }
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
+function displayWinner(choice, computerChoice) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
 
   if (
@@ -40,6 +29,21 @@ while (true) {
   } else {
     prompt('It\'s a tie');
   }
+}
+
+while (true) {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
+
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt('That\'s not a valid choice');
+    choice = readline.question();
+  }
+
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+
+  displayWinner(choice, computerChoice);
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
