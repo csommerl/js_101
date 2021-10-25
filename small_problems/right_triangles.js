df56b98c7970743a9ac1f3aa603851c2c57ab947
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2021-10-25 13:21:36 -0400

    Finish right triangles

--- small_problems/right_triangles.js
+++ small_problems/right_triangles.js
@@ -3,8 +3,21 @@
 ** https://launchschool.com/exercises/ff51259d
 */
 
-function triangle(posInt) {
-  
+// mine
+// function triangle(posInt) {
+//   for (let numOfSpaces = posInt; numOfSpaces >= 0; numOfSpaces -= 1) {
+//     console.log(' '.repeat(numOfSpaces) + '*'.repeat(posInt - numOfSpaces));
+//   }
+// }
+
+function triangle(height) {
+  let spaces = height;
+  let stars = height - spaces;
+  while (spaces >= 0) {
+    console.log(' '.repeat(spaces) + '*'.repeat(stars));
+    spaces = spaces - 1;
+    stars = height - spaces;
+  }
 }
 
 triangle(5);
