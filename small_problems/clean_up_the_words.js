a26ed9c4e05d4a4e8745cde81a0b6a2d19f9bbf4
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2021-10-26 10:48:59 -0400

    Began clean up the words

--- small_problems/clean_up_the_words.js
+++ small_problems/clean_up_the_words.js
@@ -3,3 +3,10 @@
 ** https://launchschool.com/exercises/6ccbdf07
 */
 
+function cleanUp(str) {
+  let strArr = str.split(' ');
+  let replacedCharsArr = strArr.map(word => word.replace(/\W/g, ' '));
+  return replacedCharsArr.join(' ');
+}
+
+cleanUp("---what's my +*& line?");    // " what s my line "
