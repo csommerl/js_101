f2b01c8f38445618eda43a27b4b608a828923e97
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2021-12-08 10:29:56 -0500

    Add best to five, tidied up logic

--- lesson6/twenty-one.js
+++ lesson6/twenty-one.js
@@ -1,5 +1,14 @@
 // Twenty-One https://launchschool.com/lessons/fb4809a8/assignments/62238c60
 
+/*
+** I have lots of functiond dependent on other functions,
+** which are in turn dependent on still other functions.
+** I worry that it becomes difficult to track the program's logic.
+** Should I not try to create so many functions?
+** Maybe instead I should have one main game loop, as in the sample solution?
+** Another worry: functions have multiple side effects, since they are nested.
+*/
+
 const readline = require('readline-sync');
 
 const SUITS = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
@@ -13,12 +22,13 @@ const DEALER_MINIMUM_PLAY = 17;
 const DIVIDER = '--------------------';
 const MAX_VALID_SCORE = 21;
 const GAME_NAME = 'Twenty-One';
+const GAMES_TO_WIN = 5;
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
 }
 
-function divider() {
+function divider() {  /// Question: when should global constants be passed or not to arguments?
   console.log(DIVIDER);
 }
 
@@ -129,37 +139,35 @@ function getHighestScore(cardsObj, playersArr) {
   return Math.max(...validScores);
 }
 
-function getWinners(cardsObj, playersArr, winningScore) {
-  return playersArr.filter(player => {
-    return calculateScore(cardsObj[player]) === winningScore;
-  });
+function detectWinnersAndBusted(cardsObj, playersArr) { // note that in either case, it returns an array
+  let winnerArr;
+  let bustedArr = playersArr.filter(player => busted(cardsObj[player]));
+
+  if (bustedArr.length > 0) {
+    winnerArr = playersArr.filter(player => !busted(cardsObj[player])); // this would *not* work for more than two players!
+  } else {
+    let highestScore = getHighestScore(cardsObj, playersArr);
+    winnerArr = playersArr.filter(player => {
+      return calculateScore(cardsObj[player]) === highestScore;
+    });
+  }
+
+  return [winnerArr, bustedArr];
 }
 
-function displayWinners(cardsObj, playersArr) {
-  let highestScore = getHighestScore(cardsObj, playersArr);
-  let winners = getWinners(cardsObj, playersArr, highestScore);
-
-  if (winners.length === 1) {
-    let winner = winners[0];
-    let verb = winner === 'You' ? 'win' : 'wins';
-    prompt(`${winners[0]} ${verb}!`);
-  } else {
-    prompt(`It was a tie between ${joinAnd(winners).toLowerCase()}!`);
-  }
-}
-
-function displayResults(cardsObj, playersArr) {
-  displayHands(cardsObj, PLAYERS);
-
-  if (playersArr.some(player => busted(cardsObj[player]))) {
-    let bustedPlayer = playersArr.find(player => busted(cardsObj[player]));
-    let nonBustedPlayer = playersArr.find(player => !busted(cardsObj[player])); // this would *not* work for more than two players!
-    let verb = nonBustedPlayer === 'You' ? 'win' : 'wins';
-    prompt(`${bustedPlayer} busted: ${nonBustedPlayer.toLowerCase()} ${verb}!`);
-  } else {
-    displayWinners(cardsObj, playersArr);
-  }
+function displayRoundResult(winnerArr, bustedArr) {
   divider();
+  if (bustedArr.length > 0) {
+    bustedArr.forEach(bustedPlayer => {
+      prompt(`${bustedPlayer} busted!`);
+    });
+  }
+
+  if (winnerArr.length === 1) {
+    prompt(`${winnerArr[0]} won!`);
+  } else {
+    prompt(`It was a tie between: ${joinAnd(winnerArr).toLowerCase()}.`);
+  }
 }
 
 function busted(hand) {
@@ -198,11 +206,47 @@ function dealerMove(cardsObj, player) {
   }
 }
 
-function playRound() {
-  let cards = {};
+function initializeWinsTally(playersArr) {
+  let wins = {};
+
+  playersArr.forEach(function(player) {
+    wins[player] = 0;
+  });
+
+  return wins;
+}
+
+function updateWinsTally(wins, winnerArr) {
+  if (winnerArr.length > 1) {
+    wins['ties'] = wins['ties'] ? wins + 1 : 1;  // do as short-circuit?
+  } else {
+    wins[winnerArr[0]] += 1;
+  }
+
+  return wins;
+}
+
+function playMatch() {  // do I need to pass PLAYERS?
+  let wins = initializeWinsTally(PLAYERS);
+
+  while (PLAYERS.every(player => wins[player] < GAMES_TO_WIN)) {
+    playRound(wins);
+    divider();
+    PLAYERS.forEach(player => {
+      prompt(`${player} has won ${wins[player]} so far.`);  /// fix verb
+    });
+  }
+
+  let matchWinner = PLAYERS.find(player => wins[player] === GAMES_TO_WIN);
+  divider();
+  prompt(`${matchWinner} won the match`);
+}
+
+function playRound(wins) {
+  let cards = {}; /// Question: should I just make this a global constant?
   cards.deck = initializeDeck();
 
-  dealHands(cards, PLAYERS);
+  dealHands(cards, PLAYERS);  /// Question: when should global constants be passed or not to arguments?
 
   playerMove(cards, 'You'); // if there were additional computer players, this would have to be revised
 
@@ -210,13 +254,24 @@ function playRound() {
     dealerMove(cards, 'Dealer');
   }
 
-  displayResults(cards, PLAYERS);
+  let [winnerArr, bustedArr] = detectWinnersAndBusted(cards, PLAYERS);
+  updateWinsTally(wins, winnerArr);
+
+  displayHands(cards, PLAYERS);
+  displayRoundResult(winnerArr, bustedArr);
+
+  prompt('Press any key (besides enter) to continue.');
+  readline.keyIn();
+  console.clear();
 }
 
 // Main Program
 prompt(`Welcome to ${GAME_NAME}!`);
+
 while (true) {
-  playRound();
+  prompt(`The player who is the first to win ${GAMES_TO_WIN} rounds wins the match.`);
+
+  playMatch();
 
   let playAgain;
   while (true) {
@@ -229,5 +284,6 @@ while (true) {
 
   console.clear();
 }
+
 divider();
 prompt(`Thanks for playing ${GAME_NAME}!`);
