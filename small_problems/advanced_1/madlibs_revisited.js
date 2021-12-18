715d2b507f1d1c5830afb6bda7aa8b4a22d916e4
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2021-12-18 11:57:37 -0500

    finish my version of madlibs revisited

--- small_problems/advanced_1/madlibs_revisited.js
+++ small_problems/advanced_1/madlibs_revisited.js
@@ -1,16 +1,33 @@
 // Madlibs Revisited https://launchschool.com/exercises/55f51106
 
+const REPLACEMENT_TEXTS = {
+  adjective: ['quick', 'lazy', 'sleepy', 'noisy', 'hungry'],
+  noun: ['fox', 'dog', 'head', 'leg', 'tail', 'cat'],
+  verb: ['jumps', 'lifts', 'bites', 'licks', 'pats'],
+  adverb: ['easily', 'lazily', 'noisily', 'excitedly'],
+};
+
 function madlibs(template) {
-  const REPLACEMENT_TEXTS = {
-    adjectives: ['quick', 'lazy', 'sleepy', 'noisy', 'hungry'],
-    nouns: ['fox', 'dog', 'head', 'leg', 'tail', 'cat'],
-    verbs: ['jumps', 'lifts', 'bites', 'licks', 'pats'],
-    adverbs: ['easily', 'lazily', 'noisily', 'excitedly'],
-  };
+  let keys = Object.keys(REPLACEMENT_TEXTS);
+  keys.forEach(key => {
+    let re = new RegExp(`\\b${key}\\b`);
+    while (re.test(template)) {
+      let randomIdx = Math.floor(Math.random() * REPLACEMENT_TEXTS[key].length);
+      let replacement = REPLACEMENT_TEXTS[key][randomIdx];
+      template = template.replace(re, replacement);
+    }
+  });
+
+  return template;
 }
 
-let template1 = 'The "adjective" brown "noun" "adverb" "verb" the "adjective" yellow "noun", who "adverb" "verb" his "noun" and looks around.';
-let template2 = 'The "noun" "verb" the "noun"\'s "noun".';
+let template1 =
+  'The adjective brown noun adverb ' +
+  'verb the adjective yellow ' +
+  'noun, who adverb verb his ' +
+  'noun and looks around.';
+
+let template2 = 'The noun verb the noun\'s noun.';
 
 // These examples use the following list of replacement texts:
 // adjectives: quick lazy sleepy noisy hungry
@@ -19,18 +36,18 @@ let template2 = 'The "noun" "verb" the "noun"\'s "noun".';
 // adverbs: easily lazily noisily excitedly
 // ------
 
-madlibs(template1);
+console.log(madlibs(template1));
 // The "sleepy" brown "cat" "noisily"
 // "licks" the "sleepy" yellow
 // "dog", who "lazily" "licks" his
 // "tail" and looks around.
 
-madlibs(template1);
+console.log(madlibs(template1));
 // The "hungry" brown "cat" "lazily"
 // "licks" the "noisy" yellow
 // "dog", who "lazily" "licks" his
 // "leg" and looks around.
 
-madlibs(template2);      // The "fox" "bites" the "dog"'s "tail".
+console.log(madlibs(template2));      // The "fox" "bites" the "dog"'s "tail".
 
-madlibs(template2);      // The "cat" "pats" the "cat"'s "head".
+console.log(madlibs(template2));      // The "cat" "pats" the "cat"'s "head".
