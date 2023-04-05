4da033c58f394207ac9321d732b2eb53de74a8e6
Curtis Sommerlatte <curtis.sommerlatte@gmail.com>
2023-04-05 10:01:54 -0500

    start merge sort

--- small_problems/advanced_1/mergeSort.js
+++ small_problems/advanced_1/mergeSort.js
@@ -1,9 +1,68 @@
 /*
 https://launchschool.com/exercises/968bc76d
+
+rules
+- every nested array with length > 1 must be split nested arrays.
+
+Problems:
+- how to dig into subarrays?
+- baseline condition?
+  - Every element is either an array of length 1, or
+    is an array that is fullysplit
+
+1. If the array contains non-arrays, split
+2. Othewise, for every nested array, split
 */
 
-function mergeSort(numArr) {
+/*
+Examples
 
+[9, 5, 7, 1]
+[[9, 5], [7, 1]]
+[[[9], [5]], [[7], [1]]]
+[[5, 9], [1, 7]]
+[1, 5, 7, 9]
+
+
+[6, 2, 7, 1, 4]
+[[6, 2, 7], [1, 4]]
+[[[6, 2], [7]], [[1], [4]]]
+[[[[6], [2]], [7]], [[1], [4]]]
+[[[2, 6], [7]], [1, 4]]
+[[2, 6, 7], [1, 4]]
+[1, 2, 4, 6, 7]
+
+*/
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let midpoint = Math.ceil(arr.length / 2);
+
+  let firstArr = mergeSort(arr.slice(0, midpoint));
+  let secondArr = mergeSort(arr.slice(midpoint));
+
+  return merge(firstArr, secondArr);
+}
+
+function merge(arr1, arr2) {
+  const result = [];
+
+  let [arr1Idx, arr2Idx] = [0, 0];
+
+  while (arr1Idx < arr1.length || arr2Idx < arr2.length) {
+    let [arr1Elem, arr2Elem] = [arr1[arr1Idx], arr2[arr2Idx]];
+
+    if (arr2Idx >= arr2.length || arr1Elem <= arr2Elem) {
+      result.push(arr1Elem);
+      ++arr1Idx;
+    } else {
+      result.push(arr2Elem);
+      ++arr2Idx;
+    }
+  }
+
+  return result;
 }
 
 console.log(mergeSort([9, 5, 7, 1]));           // [1, 5, 7, 9]
